d5422753ea7aad1e58809951a02938f67a380a88
Marc Bensan <marcbensan.inq@gmail.com>
2024-04-23 15:09:19 -0400

    Added enter to submit prompt feature

--- src/app/Components/ChatBox.tsx
+++ src/app/Components/ChatBox.tsx
@@ -49,6 +49,12 @@ function ChatBox(props : Props) {
       }
     }
   };
+  
+  // another way of submitting prompts (Press "Enter")
+  const handleEnterKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter')
+      handleMessageSend();
+  }
 
   const handleInputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -78,6 +84,7 @@ function ChatBox(props : Props) {
             className="form-control"
             value={inputValue}
             onChange={handleInputChange}
+            onKeyDown={handleEnterKey}
             placeholder="Type a message..."
           />
           <button className="btn btn-primary" onClick={handleMessageSend}>
