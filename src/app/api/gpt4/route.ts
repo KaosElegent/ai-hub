09a4f45cff7cc5f83929fd334f6c5bb7bc5adcac
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-04-29 01:04:35 -0400

    feat:authentication required to access the apis

--- src/app/api/gpt4/route.ts
+++ src/app/api/gpt4/route.ts
@@ -1,13 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server'; 
 import { getResponse } from '../openai';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import { checkAuth } from '../userAuth';
+import { UserProfile } from '@auth0/nextjs-auth0/client';
 
-export async function POST(req: NextRequest, res: NextResponse) {
-    try {
-        const {prompt} = await req.json();
-        const message:string = await getResponse(prompt,"gpt-3.5-turbo") || "API Down...";
-        return NextResponse.json({ message:message }, { status: 200 })
-    } catch (err) {
-        console.log("can't post: ", err);
-        return NextResponse.json({ error: `Internal Server Error: ${err}` }, { status: 500 })
+export const POST = withApiAuthRequired( async function gptAPI(req: NextRequest) {
+    const res = new NextResponse();
+    
+    const session:any = await getSession(req,res);
+
+    if(session){
+        const user:UserProfile = session.user;
+        if(user && ! await checkAuth(user.email || "")){
+            console.log(`Unauthorized Access Attempt by [${user.email}]`);
+            return NextResponse.json({ error: "User not authorized" }, { status: 401 })
+        }
+        try {
+            const {prompt} = await req.json();
+            const message:string = await getResponse(prompt,"gpt-4") || "API Down...";
+            return NextResponse.json({ message:message }, { status: 200 })
+        } catch (err) {
+            console.log("can't post: ", err);
+            return NextResponse.json({ error: `Internal Server Error: ${err}` }, { status: 500 })
+        }
     }
-}
+    return NextResponse.json({ error: `No session found.` }, { status: 401 })
+});
